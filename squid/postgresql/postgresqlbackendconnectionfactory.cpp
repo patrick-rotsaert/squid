cf94ec8c64fe4cd85f8d6bc8838080875242d90f
Patrick Rotsaert <patrick.rotsaert@dioss.com>
2022-02-22 15:50:49 +0100

    Remove obsolete license file

--- squid/postgresql/postgresqlbackendconnectionfactory.cpp
+++ squid/postgresql/postgresqlbackendconnectionfactory.cpp
@@ -1,7 +1,7 @@
 //
 // Copyright (C) 2022 Patrick Rotsaert
 // Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
+// (See accompanying file LICENSE or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
