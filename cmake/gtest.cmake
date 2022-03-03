14f7de541550d8fa499f9198534dda7b633e8c00
Patrick Rotsaert <patrick.rotsaert@dioss.com>
2022-03-03 10:03:43 +0100

    Setup unit test machinery

---
+++ cmake/gtest.cmake
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2022 Patrick Rotsaert
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+#
+
+include_guard(GLOBAL)
+
+option(${PROJECT_NAME_UC}_TEST "Build the tests" OFF)
+
+if(${PROJECT_NAME_UC}_TEST)
+	include(FetchContent)
+	FetchContent_Declare(
+	googletest
+	URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
+	)
+	# For Windows: Prevent overriding the parent project's compiler/linker settings
+	set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+	FetchContent_MakeAvailable(googletest)
+endif()
+
+include(GoogleTest)
